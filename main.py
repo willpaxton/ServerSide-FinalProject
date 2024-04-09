6a162e3f8c94f6ad766fe1a226366fd6e0026a6c
Will Paxton <wpaxton5555@gmail.com>
2024-04-08 22:46:03 -0400

    Working Docker Image

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import psutil
+import platform
+from datetime import datetime
+
+import math
+
 app = FastAPI()
 
 
@@ -25,3 +31,28 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/system_info")
+def read_cpu():
+    svmem = psutil.virtual_memory()
+
+
+    return {"total_cpu" : f"{psutil.cpu_percent()}%",
+            "total_memory" : f"{round(svmem.total/1000000000,1)}GB",
+            "percent_memory" : f"{svmem.percent}%"}
+
+# let's print CPU information
+print("="*40, "CPU Info", "="*40)
+# number of cores
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+# CPU frequencies
+cpufreq = psutil.cpu_freq()
+print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+# CPU usage
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
